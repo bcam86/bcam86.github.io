6ab2d81f77bce8225b35e14f99ad0ac0cc3f0d32
Bernardo Camargo Silva <bernardo.silva@serpro.gov.br>
2019-09-26 15:54:25 -0300

    site com gatsby

---
+++ code-report/content/meta/config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  siteTitle: "Code Report", // <title>
+  shortSiteTitle: "Code Report - shortSiteTitle", // <title> ending for posts and pages
+  siteDescription: "Code Report - siteDescription",
+  siteUrl: "https://code.report",
+  // pathPrefix: "",
+  siteImage: "preview.jpg",
+  siteLanguage: "en",
+
+  /* author */
+  authorName: "Bernardo Camargo",
+  authorTwitterAccount: "bcam86",
+
+  /* info */
+  headerTitle: "Bernardo Camargo",
+  headerSubTitle: "Code Report headerSubTitle",
+
+  /* manifest.json */
+  manifestName: "Code Report - manifestName",
+  manifestShortName: "Code Report", // max 12 characters
+  manifestStartUrl: "/index.html",
+  manifestBackgroundColor: "white",
+  manifestThemeColor: "#666",
+  manifestDisplay: "standalone",
+
+  // gravatar
+  // Use your Gravatar image. If empty then will use src/images/jpg/avatar.jpg
+  // Replace your email adress with md5-code.
+  // Example https://www.gravatar.com/avatar/g.strainovic@gmail.com ->
+  // gravatarImgMd5: "https://www.gravatar.com/avatar/1db853e4df386e8f699e4b35505dd8c6",
+  gravatarImgMd5: "",
+
+  // social
+  authorSocialLinks: [
+    { name: "github", url: "https://github.com/bcam86.github.io" },
+    { name: "twitter", url: "https://twitter.com/bcam86" },
+    { name: "facebook", url: "http://facebook.com/greglobinski" }
+  ]
+};
